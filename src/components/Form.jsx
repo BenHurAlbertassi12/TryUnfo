71c33c8e17b7b5287d5874bad01a9ad0d4374e92
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-08-23 15:47:38 -0300

    Arrumando o README

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,34 +5,41 @@ class Form extends React.Component {
     return (
       <form>
         <label htmlFor="req1-1">
+          Nome
           <input type="text" data-testid="name-input" id="req1-1" />
         </label>
 
         <label htmlFor="req1-2">
+          Descrição
           <textarea data-testid="description-input" id="req1-2" />
         </label>
 
         <label htmlFor="req1-3">
+          Atributo 1
           <input data-testid="attr1-input" type="number" id="req1-3" />
         </label>
 
         <label htmlFor="req1-4">
+          Atributo 2
           <input data-testid="attr2-input" type="number" id="req1-4" />
         </label>
 
         <label htmlFor="req1-5">
+          Atributo 3
           <input data-testid="attr3-input" type="number" id="req1-5" />
         </label>
 
         <label htmlFor="req1-6">
+          Imagem
           <input type="text" data-testid="image-input" src="" id="req1-6" />
         </label>
 
         <label htmlFor="req1-7">
+          Raridade
           <select data-testid="rare-input" name="" id="req1-7">
-            <option value="normal">Normal</option>
-            <option value="raro">Raro</option>
-            <option value="muito raro">Muito Raro</option>
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
           </select>
         </label>
 
@@ -40,8 +47,10 @@ class Form extends React.Component {
           <input type="checkbox" id="req1-8" />
         </label>
 
-        <input type="button" data-testid="save-button" />
-
+        <label htmlFor="req1-9">
+          Super Trunfo?
+          <input id="req1-9" type="button" data-testid="save-button" />
+        </label>
       </form>
     );
   }
